81630c04c5fcc377aff6298eb0e0443b0af70890
Laura <pizzuttilaura@gmail.com>
2024-07-29 12:27:52 -0300

    commit

--- back/src/routes/tabelaRouter.js
+++ back/src/routes/tabelaRouter.js
@@ -4,8 +4,8 @@ const router = Router();
 const { storeItem, getItems, deleteItems } = require('../controller/tabelaController');
 
 
-router.post('/api/store/tabela', storeItem); 
+router.post('/store/tabela', storeItem); 
 router.get('/get/tabela', getItems);   
-router.delete('/api/delete/tabela', deleteItems);
+router.delete('/delete/tabela', deleteItems);
 
 module.exports = router;
