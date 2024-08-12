59946ffee7558d4f11058b9ee5165b9b911f6994
Laura <pizzuttilaura@gmail.com>
2024-08-12 12:25:13 -0300

    id doador

--- front/html/js/agendamento.js
+++ front/html/js/agendamento.js
@@ -1,42 +1,42 @@
-async function getTask() {
-    const Id_User = localStorage.getItem('id');
+// async function getTask() {
+//     const Id_User = localStorage.getItem('id');
 
-    const response = await fetch('http://localhost:3001/api/itens/'+ Id_User, {
-        method: "GET",
-        headers: {
-            "Content-Type":"application/json"
-        }
-    })
+//     const response = await fetch('http://localhost:3001/api/itens/'+ Id_User, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type":"application/json"
+//         }
+//     })
 
-    const results = await response.json();
-    console.log('ID do usuário: ', Id_User)
-    console.log(results)
-    if(results.success) {
-        let itens = results.data;
+//     const results = await response.json();
+//     console.log('ID do usuário: ', Id_User)
+//     console.log(results)
+//     if(results.success) {
+//         let itens = results.data;
 
-        let tabela = document.getElementById('tabela');
+//         let tabela = document.getElementById('tabela');
 
-        itens.map(item => {
-            let htmlItem = document.createElement('div');
-            htmlItem.classList.add('opcao');
+//         itens.map(item => {
+//             let htmlItem = document.createElement('div');
+//             htmlItem.classList.add('opcao');
     
-            const quantidadeSpan = document.createElement('span');
-            quantidadeSpan.classList.add('quantidade');
-            quantidadeSpan.textContent = `${item.qnt_itens}x`;
+//             const quantidadeSpan = document.createElement('span');
+//             quantidadeSpan.classList.add('quantidade');
+//             quantidadeSpan.textContent = `${item.qnt_itens}x`;
     
-            const nomeSpan = document.createElement('span');
-            nomeSpan.classList.add('item');
-            nomeSpan.textContent = item.item;
+//             const nomeSpan = document.createElement('span');
+//             nomeSpan.classList.add('item');
+//             nomeSpan.textContent = item.item;
     
-            htmlItem.appendChild(quantidadeSpan);
-            htmlItem.appendChild(nomeSpan); 
+//             htmlItem.appendChild(quantidadeSpan);
+//             htmlItem.appendChild(nomeSpan); 
 
-            tabela.appendChild(htmlItem);
-        });
-        if (adicionarItem.style.display === 'block') {
-            Excluir(true);
-        }
-    }
-}
+//             tabela.appendChild(htmlItem);
+//         });
+//         if (adicionarItem.style.display === 'block') {
+//             Excluir(true);
+//         }
+//     }
+// }
 
-getTask();
+// getTask();
