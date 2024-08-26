ec59db1bcb0ff180f4250ab139942c6a997cc1fb
Laura <pizzuttilaura@gmail.com>
2024-08-26 12:22:48 -0300

    banco

--- banco/Banco de Dados.sql
+++ banco/Banco de Dados.sql
@@ -39,9 +39,3 @@ INSERT INTO cadastro_usuario(nome, email, senha,tipo_usuario,endereco)VALUES('ON
 INSERT INTO cadastro_usuario(nome, email, senha,tipo_usuario,endereco)VALUES('Laura Pizzuti','lalapizzutti@gmail.com','Laura','Colaborador','Rua 63, Rio Verde');
 INSERT INTO tabela_itens(item,qnt_itens,id_user) VALUES('Arroz', 8, 1);
 INSERT INTO agendamentos (id, item, data_entrega, hora_entrega, qnt, id_doador, id_insti, _status) VALUES ('1','Arroz', '2022-06-06','09:00', 10, 2, 1, 'Agendada');
-
--- DELETE FROM tabela_itens;
--- DELETE FROM cadastro_usuario WHERE email = 'fyjycrf';
--- DROP TABLE cadastro_usuario;
-
--- truncate table agendamentos;
